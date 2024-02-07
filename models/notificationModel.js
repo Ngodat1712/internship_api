2c1764c4e9f7fe5abcd3b7c877ae4da0ece6c96e
Ngodat1712 <dataiaka90@gmail.com>
2024-02-07 20:50:25 +0700

    push

---
+++ models/notificationModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const notificationModal = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    notifications: [
+      {
+        title: String,
+        body: String,
+        companyName: String,
+        jobTitle: String,
+      },
+    ],
+  },
+  { timestamp: true }
+);
+
+module.exports = mongoose.model("Notification", notificationModal);
